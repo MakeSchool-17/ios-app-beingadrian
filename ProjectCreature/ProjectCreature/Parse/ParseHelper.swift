0c8b86f70aeefdce58bef407f4e622e68ba558b7
Adrian Wisaksana <adrian.wisaksana@students.makeschool.com>
2015-11-13 14:49:53 -0800

    Implement Parse

---
+++ ProjectCreature/ProjectCreature/Parse/ParseHelper.swift
@@ -0,0 +1,17 @@
+//
+//  ParseHelper.swift
+//  ProjectCreature
+//
+//  Created by Adrian Wisaksana on 11/13/15.
+//  Copyright © 2015 BeingAdrian. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+
+class ParseHelper {
+    
+    // insert code here
+    
+}
