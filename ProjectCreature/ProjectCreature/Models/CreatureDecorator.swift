0a64880ec6e57031c4a1bf3a3879d45478fa4bbd
Adrian Wisaksana <adrian.wisaksana@students.makeschool.com>
2015-11-24 22:53:06 -0800

    Update mockup

---
+++ ProjectCreature/ProjectCreature/Models/CreatureDecorator.swift
@@ -0,0 +1,40 @@
+//
+//  CreatureDecorator.swift
+//  ProjectCreature
+//
+//  Created by Adrian Wisaksana on 11/24/15.
+//  Copyright © 2015 BeingAdrian. All rights reserved.
+//
+
+import Foundation
+import PromiseKit
+
+
+class CreatureDecorator {
+    
+    let creature: Creature
+    
+    init(creature: Creature) {
+        
+        self.creature = creature
+        
+    }
+    
+    
+    // MARK: - Stats
+    
+    func increaseHappinessBy(value: Int) {
+        
+        
+        
+    }
+    
+    func decreaseHappinessBy(value: Int) {
+        
+        
+        
+    }
+    
+        
+    
+}
